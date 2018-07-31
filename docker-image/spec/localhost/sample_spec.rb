58002c9b3bd7420b3b02bd398a513f79979b5fea
maemichi-monosense <maemichi@monosense.co.jp>
2018-07-31 15:49:25 +0900

    Add spec on OS & AWS CLI

--- docker-image/spec/localhost/sample_spec.rb
+++ docker-image/spec/localhost/sample_spec.rb
@@ -1,28 +1,12 @@
 require 'spec_helper'
 
-describe package('httpd'), if: os[:family] == 'redhat' do
-  it { should be_installed }
-end
+describe 'image' do
+  describe 'Amazon Linux' do
+    it { expect(os[:family]).to eq 'amazon' }
+    it { expect(os[:release]).to eq '2018.03' }
+  end
 
-describe package('apache2'), if: os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
-
-describe service('httpd'), if: os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('apache2'), if: os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('org.apache.httpd'), if: os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe port(80) do
-  it { should be_listening }
+  describe package('aws-cli') do
+    it { should be_installed } 
+  end
 end
